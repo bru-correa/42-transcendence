3b419987bb0ce9adcb3bfa541dc62a6cc3e35e34
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2024-05-31 15:55:42 -0300

    :sparkles: feat: setup nginx

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,3 +6,6 @@ bind = "0.0.0.0:8000"
 wsgi_app = "transcendence.wsgi:application"
 
 workers = multiprocessing.cpu_count() * 2 + 1
+
+certfile = "./certs/localhost.crt"
+keyfile = "./certs/localhost.key"
