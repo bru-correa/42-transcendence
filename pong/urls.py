57c3cb88d57d2d17bafdacbcb639d016022f0d84
bru-correa <bruuh.cor@gmail.com>
2024-06-02 17:51:56 -0300

    feat: add basic page views

--- pong/urls.py
+++ pong/urls.py
@@ -2,6 +2,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("game", views.game, name="game")
+    path("", views.get_home_page, name="home-page"),
+    path("login", views.get_login_page, name="login-page"),
+    path("game", views.get_game_page, name="game-page"),
+    path("social", views.get_social_page, name="social-page"),
+    path("stats", views.get_stats_page, name="stats-page"),
+    path("tournament", views.get_tournament_page, name="tournament-page")
 ]
--- pong/views.py
+++ pong/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import HttpResponse, render
 
 # Create your views here.
-def game(request):
+def get_home_page(request):
+    return render(request, "pages/home.html")
+
+def get_login_page(request):
+    return render(request, "pages/login.html")
+
+def get_game_page(request):
     return render(request, "pages/game.html")
 
-def home(request):
-    return render(request, "pages/home.html")
+def get_social_page(request):
+    return render(request, "pages/social.html")
+
+def get_stats_page(request):
+    return render(request, "pages/stats.html")
+
+def get_tournament_page(request):
+    return render(request, "pages/tournament.html")
