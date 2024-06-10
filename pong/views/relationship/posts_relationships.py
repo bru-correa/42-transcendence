e080130424459e8e53dcf05ef4627f8beae453a8
B-Mugnol <bruno_mugnol@hotmail.com>
2024-06-10 03:46:37 -0300

    :sparkles: feats: add endpoints for accepting and denying friend requests, and removing friends

--- pong/views/relationship/posts_relationships.py
+++ pong/views/relationship/posts_relationships.py
@@ -4,10 +4,78 @@ from django.http import HttpRequest, JsonResponse
 from pong.models import Relationship, User
 from .queries_relationship import *
 
-# TODO
-# cancel_friend_request
-# accept_friend_request
-# deny_friend_request
+@login_required(login_url="/login")
+def deny_friend_request(request: HttpRequest):
+	return remove_friend(request)
+
+@login_required(login_url="/login")
+def remove_friend(request: HttpRequest):
+	if request.method != "POST":
+		return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+	friend_id = request.POST.get('friend_id')
+	try:
+		if friend_id is None:
+			raise Exception("'friend_id' is empty")
+		if not isinstance(request.user, User):
+			raise Exception("Authentication failed to provide a valid user")
+
+		friend = User.objects.get(pk=friend_id)
+		user = request.user
+
+		friendship = Relationship.objects.get(
+			Q(user1=user, user2=friend) | Q(user1=friend, user2=user)
+		)
+		friendship.delete()
+
+	except Exception as e:
+		return JsonResponse({
+			'success': False,
+			'message': f'{e}'
+		}, status=400)
+
+	return JsonResponse({
+		'success': True,
+		'message': 'Friend removed'
+	})
+
+@login_required(login_url="/login")
+def accept_friend_request(request: HttpRequest):
+	if request.method != "POST":
+		return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+	friend_id = request.POST.get('friend_id')
+	try:
+		if friend_id is None:
+			raise Exception("'friend_id' is empty")
+		if not isinstance(request.user, User):
+			raise Exception("Authentication failed to provide a valid user")
+
+		friend = User.objects.get(pk=friend_id)
+		user = request.user
+		is_friend = Relationship.objects.filter(
+			Q(user1=user.pk,user2=friend.pk) | Q(user1=friend.pk,user2=user.pk),
+			user1_is_friendly=True, user2_is_friendly=True
+			).exists()
+		if is_friend:
+			raise Exception("Already friends")
+		if get_sent_requests_qlist(user).filter(user2=friend.pk):
+			raise Exception("A friend request was already sent by you to this user")
+
+		friendship = Relationship.objects.get(user1=friend.pk, user2=user.pk)
+		friendship.user2_is_friendly = True
+		friendship.save()
+
+	except Exception as e:
+		return JsonResponse({
+			'success': False,
+			'message': f'{e}'
+		}, status=400)
+
+	return JsonResponse({
+		'success': True,
+		'message': 'Friend request accepted'
+	})
 
 @login_required(login_url="/login")
 def send_friend_request(request: HttpRequest):
@@ -17,7 +85,7 @@ def send_friend_request(request: HttpRequest):
 	friend_id = request.POST.get('friend_id')
 	try:
 		if friend_id is None:
-			raise Exception("'name' is empty")
+			raise Exception("'friend_id' is empty")
 		if not isinstance(request.user, User):
 			raise Exception("Authentication failed to provide a valid user")
 
